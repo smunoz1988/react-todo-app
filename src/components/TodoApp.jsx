ab3ed6b29eff7546a8fe65be8a679694ac141093
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-02 17:16:20 -0500

    Setup id generator, update editItem function

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,11 +2,14 @@ import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
 
 const TodoApp = () => {
-    return (
-      <>
+  return (
+    <div className="wrapper">
+      <div className="todos">
         <Header />
         <TodosLogic />
-      </>
-    );
-  };
-  export default TodoApp;
+      </div>
+    </div>
+  );
+};
+export default TodoApp;
+
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 import InputTodo from '@/components/InputTodo';
 import TodosList from '@/components/TodosList';
 
@@ -6,17 +7,17 @@ import TodosList from '@/components/TodosList';
 const TodosLogic = () => {
     const [todos, setTodos] = useState([
         {
-          id: 1,
+          id: uuidv4(),
           title: 'Setup development environment',
           completed: true,
         },
         {
-          id: 2,
+          id: uuidv4(),
           title: 'Develop website and add content',
           completed: false,
         },
         {
-          id: 3,
+          id: uuidv4(),
           title: 'Deploy to live server',
           completed: false,
         },
@@ -45,17 +46,28 @@ const TodosLogic = () => {
 
     const addTodoItem = (title) => {
         const newTodo = {
-            id: 4,
+            id: uuidv4(),
             title: title,
             completed: false,
         };
         setTodos([...todos, newTodo]);
     };
 
+    const setUpdate = (updatedTitle, id) => {
+        setTodos(
+          todos.map((todo) => {
+            if (todo.id === id) {
+              todo.title = updatedTitle;
+            }
+            return todo;
+          })
+        );
+    };
+
     return (
         <div>
             <InputTodo addTodoItem={addTodoItem} />
-            <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
+            <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} setUpdate={setUpdate}/>
         </div>
     );
   };
